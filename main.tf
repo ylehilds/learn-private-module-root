b05d03b05392c9ad574beccd0144d1108831d0b7
ylehilds <lehi_alcantara@byu.edu>
2023-05-09 01:46:35 -0600

    finished terraform AWS Get Started & Modules

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 4.0.0"
+#    }
+#  }
+#}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name    = var.name
-  region  = var.region
-  prefix  = var.prefix
-  version = "1.0.0"
+  source  = "github.com/ylehilds/terraform-aws-s3-webapp?ref=v1.0.2"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
 }
